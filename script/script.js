21fc6f380bd21e69708f4fa933a9b63c8f12729f
qwertz0-afk <ma.cherqaoui2012@gmail.com>
2026-07-06 00:11:59 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,106 @@
+const firstbtn = document.getElementById("firstbutton");
+const weatherbutton = document.getElementById("weatherbtn");
+const weatherwidget = document.getElementById("weatherbloc");
+const curtain = document.getElementById("curtain");
+const leftarrow = document.getElementById("arrow_left");
+const warnT = document.getElementById("warn");
+const passwordinput = document.getElementById("pass");
+const circle = document.getElementById("circle");
+if (curtain) {
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            curtain.classList.replace("curtain_in", "curtain_out");
+        });
+    });
+}
+if (passwordinput) {
+    passwordinput.addEventListener("keyup", (e) => {
+        if (e.key == "Enter") {
+            passcheck()
+        }
+    })
+}
+if (firstbtn) {
+    firstbtn.addEventListener("mousedown", () => {
+        passcheck()
+});
+}
+if (leftarrow) {
+    leftarrow.addEventListener("mousedown", () => {
+        curtain.classList.replace("curtain_out", "curtain_in");
+        setTimeout(() => {
+            window.location.replace("index.html");
+        }, 750);
+});
+}
+if (window.location.pathname.includes("page2.html")) {
+    setInterval(weatherupdate(), 300000);
+}
+if (weatherbutton) {
+    axios.get('http://localhost:3000/weather')
+    .then(response => {
+        const answer = response.data;
+        if (answer == "on") {
+            weatherwidget.classList.replace("opacity-0", "opacity-100");
+        }
+        else {
+            weatherwidget.classList.replace("opacity-100", "opacity-0");
+        }
+    })
+        weatherbutton.addEventListener("mousedown", () => {
+            if (weatherwidget.classList.contains("opacity-0")) {
+                weatherwidget.classList.replace("opacity-0", "opacity-100");
+                axios.post('http://localhost:3000/weather_shown');
+            }
+            else {
+                weatherwidget.classList.replace("opacity-100", "opacity-0");
+                axios.post('http://localhost:3000/weather_hidden');
+            }
+        });
+    }   
+function weatherupdate() {
+    if (circle) {
+        circle.classList.remove("opacity-0");
+    }
+    axios.get('http://localhost:3000/getweather')
+    .then(response => {
+        circle.classList.add("opacity-0");
+        const json_answer = response.data;
+        const countryNcity = json_answer.countrycity;
+        const temperature = json_answer.temp;
+        const desc = json_answer.description;
+
+        document.getElementById("countryNcity").innerText = countryNcity;
+        document.getElementById("temperature").innerText = temperature;
+        document.getElementById("description").innerText = desc;
+    })
+}
+function passcheck() {
+    const password = passwordinput.value;
+    if (password == "") {
+        warnT.textContent = "Provide a password";
+        warnT.classList.replace("opacity-0", "opacity-100");
+        warnT.classList.remove("absolute");
+    }
+    else {
+        axios.post('http://localhost:3000', password, {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+        })
+        .then(response => {
+            const decision = response.data;
+            if (decision == 'yes') {
+                curtain.classList.replace("curtain_out", "curtain_in");
+                setTimeout(() => {
+                    window.location.replace("page2.html");
+                }, 750);
+            }
+            else {
+                warnT.textContent = "Password is incorrect";
+                warnT.classList.replace("opacity-0", "opacity-100");
+                warnT.classList.remove("absolute");
+            }
+        })
+    }
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());          
+app.use(express.text());  
+
+let weather = "off";
+
+app.post('/', (request, response) => {
+    const receivedData = request.body;
+    let decision = "no";
+    if (receivedData == "amine") {
+        decision = "yes";
+    }
+    response.send(decision);
+});
+app.get('/weather', (request, response) => {
+    let res;
+    if (weather == "off") {
+        res = "off";
+    }
+    else {
+        res = "on";
+    }
+    response.send(res);
+});
+app.post('/weather_shown', (request, response) => {
+    weather = "on";
+    response.sendStatus(204);
+});
+app.post('/weather_hidden', (request, response) => {
+    weather = "off";
+    response.sendStatus(204);
+});
+app.get('/getweather', async (request, response) => {
+    try {
+        const apiResponse = await fetch("https://wttr.in/?format=j1");
+        if (!apiResponse.ok) {
+            throw new Error("error");
+        }
+        const data = await apiResponse.json();
+        const country = data.nearest_area[0].country[0].value;
+        const city = data.nearest_area[0].areaName[0].value;
+        const temp = data.current_condition[0].temp_C;
+        const description = data.current_condition[0].weatherDesc[0].value;
+
+        const countryNcitystring = `${country}, ${city}`;
+        const tempstring= `${temp} °C`;
+        const descriptionstring = `${description}`;
+
+        const weatherJson = {
+            countrycity: countryNcitystring,
+            temp: tempstring,
+            description: descriptionstring
+        };
+        response.json(weatherJson);
+    } catch(error) {
+       console.error("Weather update failed :", error.message);
+    }
+    
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
